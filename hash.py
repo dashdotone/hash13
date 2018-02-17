b276af3bf3f07ede6e8404bb4ffff1256b281ca5
dashdotone <36506916+dashdotone@users.noreply.github.com>
2018-02-17 10:36:49 +0000

    Add files via upload

---
+++ hash.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import urllib, urllib2, cookielib
+import hashlib
+import re
+
+from datetime import datetime
+
+log_url = 'https://ringzer0team.com/login'
+ch_url = 'https://ringzer0team.com/challenges/13'
+uname = 'dashdotone'
+passwd = 'B3NzaC1yB3NzaC1y'
+
+cj = cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+login_data = urllib.urlencode({'username':uname,'password':passwd})
+opener.open(log_url,login_data)
+resp = opener.open(ch_url)
+starttime = datetime.now()
+text = resp.read().strip().splitlines()
+msg = text[text.index('\t\t----- BEGIN MESSAGE -----<br />')+1].replace('<br />','').strip()
+hash_msg = hashlib.sha512(msg).hexdigest()
+resp = opener.open(ch_url+'/'+hash_msg)
+print(datetime.now()-starttime)
+print(resp.read().splitlines()[70])
